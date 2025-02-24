e4bc3eea98eef8510afea30bfc699ed3a783b51c
MaoMachado <maogrr@yahoo.com>
2025-02-24 15:09:31 -0500

    Se agrego los archivos necesarios eh importación de fuentes

---
+++ src/assets/fotosFloresta/dataImg.js
@@ -0,0 +1,17 @@
+export const data = [
+  {
+    id: 1, imgUrl: 'src/assets/fotosFloresta/img_1.jpg'
+  },
+  {
+    id: 2, imgUrl: 'src/assets/fotosFloresta/img_2.jpg'
+  },
+  {
+    id: 3, imgUrl: 'src/assets/fotosFloresta/img_3.jpg'
+  },
+  {
+    id: 4, imgUrl: 'src/assets/fotosFloresta/img_4.jpg'
+  },
+  {
+    id: 5, imgUrl: 'src/assets/fotosFloresta/img_5.jpg'
+  }
+]
