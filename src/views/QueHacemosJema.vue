d55fa8239361e554913dd081b9377cced5b39b3e
MaoMachado <114309283+MaoMachado@users.noreply.github.com>
2025-09-15 11:00:36 -0500

    Update: QueHacemos

--- src/views/QueHacemosJema.vue
+++ src/views/QueHacemosJema.vue
@@ -1,6 +1,29 @@
 <script setup>
 import J from "@/assets/img/J.png"
 const title = "¿Qué Hacemos?"
+
+const listDetails = [
+  {
+    id: "01",
+    title: "Venta De Propiedad",
+    description: "Encuentra la propiedad ideal para ti. Gestionamos la venta de inmuebles con asesoría confiable y acompañamiento en todo el proceso."
+  },
+  {
+    id: "02",
+    title: "Compra De Propiedad",
+    description: "Te acompañamos en cada paso para que encuentres y compres la propiedad que siempre soñaste, con seguridad y confianza."
+  },
+  {
+    id: "03",
+    title: "Reformamos Tu Propiedad",
+    description: "Transformamos tu espacio con diseños modernos y materiales de calidad, aumentando el valor y la comodidad de tu propiedad."
+  },
+  {
+    id: "04",
+    title: "Inversiones",
+    description: "Descubre oportunidades seguras en el mercado inmobiliario para hacer crecer tu capital con asesoría especializada."
+  }
+]
 </script>
 
 <template>
@@ -35,7 +58,15 @@ const title = "¿Qué Hacemos?"
       </p>
     </article>
 
-
+    <article class="list-details">
+      <div class="list-details-item" v-for="details in listDetails" :key="details.id">
+        <header class="list-details-item-header">
+          <span>{{ details.id }}</span>
+          <h3>{{ details.title }}</h3>
+        </header>
+        <p>{{ details.description }}</p>
+      </div>
+    </article>
   </section>
 </template>
 
@@ -44,30 +75,29 @@ const title = "¿Qué Hacemos?"
   display: grid;
   place-content: center;
   width: var(--width-1200);
-  height: 100dvh;
+  min-height: 100dvh;
   margin-inline: auto;
   position: relative;
-  overflow: hidden;
 
   &::before {
     content: "";
-    width: 400px;
-    height: 400px;
-    background: rgba(30, 64, 175, 0.2);
-    position: absolute;
-    transform: translateX(100%) translateY(100%);
-    z-index: -1;
+    width: 200px;
+    height: 200px;
+    background: rgba(30, 64, 175, 0.1);
     border-radius: 50%;
     filter: blur(5px);
-    box-shadow: 0 0 20px hsla(0, 0%, 80%, 0.3);
-    opacity: 0.5;
+    position: absolute;
+    z-index: -1;
+    top: 70%;
+    left: 50%;
+    transform: translate(-50%, -50%);
   }
 
   & .proyectos-header {
     display: flex;
     flex-direction: column;
     align-items: center;
-    margin-bottom: 2rem;
+    margin-bottom: 1rem;
     position: relative;
 
     &::before {
@@ -76,7 +106,7 @@ const title = "¿Qué Hacemos?"
       height: 5px;
       background: linear-gradient(to right, rgba(59, 131, 246, 0.1), rgb(59, 130, 246));
       position: absolute;
-      bottom: -1rem;
+      bottom: -0.5rem;
       border-radius: 1rem;
     }
 
@@ -107,6 +137,47 @@ const title = "¿Qué Hacemos?"
       text-align: center;
       font-size: 1.2em;
       font-weight: lighter;
+    }
+  }
+}
+
+.list-details {
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+  gap: 1rem;
+
+  & .list-details-item {
+    display: flex;
+    flex-direction: column;
+    gap: 0.5rem;
+    padding: 0.5rem;
+    border: 2px solid rgba(59, 131, 246, 0.2);
+    border-radius: 10px;
+
+    &:hover {
+      box-shadow: 0 0 10px rgb(59, 130, 246);
+    }
+
+    & .list-details-item-header {
+
+      & span {
+        padding: 2.5px;
+        background: rgba(59, 131, 246, 0.2);
+        border-radius: 5px;
+        opacity: 0.5;
+        font-size: 0.8em;
+        letter-spacing: 5px;
+      }
+
+      & h3 {
+        font-size: 2em;
+        line-height: 28px;
+      }
+    }
+
+    p {
+      font-weight: lighter;
+      text-wrap: balance;
     }
   }
 }
